88d7060131899e2ecf2c5a0554758542b3dfe847
estela <jiao358@qq.com>
2024-11-22 22:21:25 +0800

    修复

--- neko-server/src/main/java/com/neko/entity/KnowledgeData.java
+++ neko-server/src/main/java/com/neko/entity/KnowledgeData.java
@@ -1,17 +1,21 @@
 package com.neko.entity;
 
 import lombok.Data;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
-public class KnowledgeData {
+public class KnowledgeData implements Serializable {
+    private Integer status;
+    private String processImage;
+    // ... 其他字段
     private Long id;
     private String contentName;
     private String contentCategory;
     private String contentUrl;
     private String contentSummary;
-    private String processImage;
-    private Integer status;
+
     private LocalDateTime createTime;
     private LocalDateTime updateTime;
+
 } 
